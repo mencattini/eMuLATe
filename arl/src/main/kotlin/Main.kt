a9815dfbd6b99984faf359c335571d6209274634
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-09-12 12:11:38 +0200

    add the computeFt function, NEED TO BE FIXED : DOESN'T WORK

--- arl/src/main/kotlin/Main.kt
+++ arl/src/main/kotlin/Main.kt
@@ -2,7 +2,8 @@ import machine.learning.ARL
 
 fun main(args : Array<String>) {
 
-    val array : DoubleArray = doubleArrayOf(1.99, 1.88, 1.77, 1.66, 1.55, 1.44, 1.33, 1.22)
+    val array : DoubleArray = doubleArrayOf(1.99, 1.88, 1.77, 1.66, 1.55, 1.66, 1.77, 1.88)
+
     var arl = ARL(array, 0.0, 3)
     arl.trainingLoop()
     println(arl.toString())
--- arl/src/main/kotlin/machine/learning/ARL.kt
+++ arl/src/main/kotlin/machine/learning/ARL.kt
@@ -16,7 +16,7 @@ class ARL(private var prices: DoubleArray, private val vThreshold: Double, priva
     private var z: Double
     private var weights : Array<Weights>
     private var parameters : Parameters
-    private var ft: Array<Pair<Double, Double>>
+    private var ft: Array<Pair<Double, Double>> // where first = the sign, second = the value
     private var returns: DoubleArray
 
     init {
@@ -54,10 +54,10 @@ class ARL(private var prices: DoubleArray, private val vThreshold: Double, priva
     /**
      * In the function we will improve the weights and parameters. This is the learning phase.
      */
-    public fun mainLoop() {
+    public fun trainingLoop() {
 
         var oldPrice = prices[0]
-        var givenT = 0
+        var givenT = 1
         // the training is done over every prices. From the soonest to the latest.
         for (price in prices.sliceArray(1..(prices.size - 1))) {
 
@@ -69,6 +69,7 @@ class ARL(private var prices: DoubleArray, private val vThreshold: Double, priva
             returns = returns.plus(computedReturn)
 
             // compute the Ft
+            ft = ft.plus(computeFt(givenT))
 
             // update the weights
 
@@ -76,67 +77,60 @@ class ARL(private var prices: DoubleArray, private val vThreshold: Double, priva
         }
     }
 
-    override fun toString(): String {
-        return "ARL(returns=${Arrays.toString(returns)})"
-    }
-
 
     /**
-//     * Compute the Ft layer using weights, vthresholrd, returns and old_ft.
-//     * Given t, my need are :
-//     * - compute ft
-//     * - return the value and the sign
-//     *
-//     * @param givenT an Int. It's our index.
-//     * @return a pair of value and signum
-//     */
-//    private fun computeFt(givenT : Int) : Pair<Double, Double> {
-//
-//        // if the t > returns.size, we are out of the array and we
-//        // needs more samples
-//        when {
-//            givenT > returns.size - 1 ->
-//                throw Exception("t($givenT) is greeter than returns.size(" + returns.size.toString() + ").")
-//            givenT > ft.size ->
-//                throw Exception("t($givenT) is greeter than ft.size(" + ft.size.toString() + ").")
-//        }
-//
-//        // this part doesn't depends on index
-//        var sum = weights[givenT] * ft[givenT - 1] + vThreshold
-//
-//
-//        // we get the useful weights and returns
-//        val usefulWeights :DoubleArray
-//        val usefulReturns :DoubleArray
-//
-//        // if the t is smaller than our windows, we just take the t first elements
-//        // we sub 2 because the last weight is used with the F_{t-1}
-//        if (givenT < sizeWindow - 2) {
-//            // as said in the formel neural net layer :
-//            // w_{i,t} * r_{t-i}
-//            // so we need to reverse the returns array
-//            usefulWeights = weights.sliceArray(0..givenT)
-//            usefulReturns = returns.sliceArray(0..givenT).reversedArray()
-//
-//        } else {
-//            // we sub the (sizeWindow - 2) to always get the same number of elements than the weights
-//            // using the maxof(0, t-sizedwindow + 2) to avoid negative index
-//            // reverse the array for the same thing than above
-//            usefulWeights = weights.sliceArray(0..(sizeWindow - 2))
-//            usefulReturns = returns.sliceArray(maxOf(0,givenT-sizeWindow + 2)..givenT).reversedArray()
-//        }
-//
-//        // we zip the two array together and do the multiplication/sum
-//        for ((wi, ri) in usefulWeights.zip(usefulReturns)) {
-//            sum += wi * ri
-//        }
-//
-//        // we add the result to the ft vector
-//        ft[givenT - 1] = Math.signum(sum)
-//
-//        return Pair(sum, Math.signum(sum))
-//    }
+     * Compute the Ft layer using weights, vthresholrd, returns and old_ft.
+     * Given t, my need are :
+     * - compute ft
+     * - return the value and the sign
+     *
+     * @param givenT an Int. It's our index.
+     * @return a pair of signum and value
+     */
+    private fun computeFt(givenT : Int) : Pair<Double, Double> {
 
+        // this part doesn't depends on index
+        var sum = weights[givenT - 1].coefficients.last() * ft[givenT - 1].first + vThreshold
+
+
+        // we get the useful weights and returns
+        val usefulWeights :DoubleArray
+        val usefulReturns :DoubleArray
+
+        // if the t is smaller than our windows, we just take the t first elements
+        // we sub 2 because the last weight is used with the F_{t-1}
+        if (givenT < sizeWindow - 2) {
+            // as said in the formal neural net layer :
+            // w_{i,t} * r_{t-i}
+            // so we need to reverse the returns array
+            usefulWeights = weights[givenT - 1].coefficients.sliceArray(0..givenT)
+            usefulReturns = returns.sliceArray(0..givenT).reversedArray()
+
+        } else {
+            // we sub the (sizeWindow - 2) to always get the same number of elements than the weights
+            // using the maxof(0, t-sizedwindow + 2) to avoid negative index
+            // reverse the array for the same thing than above
+            usefulWeights = weights[givenT - 1].coefficients.sliceArray(0..(sizeWindow - 2))
+            usefulReturns = returns.sliceArray(maxOf(0,givenT-sizeWindow + 2)..givenT).reversedArray()
+        }
+
+        // we zip the two array together and do the multiplication/sum
+        for ((wi, ri) in usefulWeights.zip(usefulReturns)) {
+            sum += wi * ri
+        }
+
+        // we reutrn the result to the ft vector
+        return Pair(Math.signum(sum), sum)
+    }
+
+    override fun toString(): String {
+        return "ARL(" +
+                "parameters=$parameters,\n" +
+                "ft=${Arrays.toString(ft)},\n" +
+                "returns=${Arrays.toString(returns)}\n," +
+                "weigths=${Arrays.toString(weights.last().coefficients)}"
+                ")"
+    }
 
 
 }
