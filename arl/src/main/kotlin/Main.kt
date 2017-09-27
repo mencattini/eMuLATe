faf34c7c46e498a5df4195aa4a9783de251bd05a
Romain Mencattini <romain.mencattini@zaclys.net>
2017-09-27 12:40:33 +0200

    Add other value in the main function

--- arl/src/main/kotlin/Main.kt
+++ arl/src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import machine.learning.ARL
 
 fun main(args : Array<String>) {
 
-    val array = arrayListOf<Double>(1.99, 1.88, 1.77, 1.66, 1.55, 1.66, 1.77, 1.88)
+    val array = arrayListOf<Double>(1.99, 1.88, 1.77, 1.66, 1.55, 1.66, 1.77, 1.88, 1.77, 1.66, 1.55, 1.44, 1.33, 1.22, 1.11, 1.00)
 
     var arl = ARL(array, 0.0, 4)
     arl.trainingLoop()
