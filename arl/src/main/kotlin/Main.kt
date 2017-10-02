82a71a00133acd2f9ce958e6bf0a4aedd0e2b8e3
Romain Mencattini <romain.mencattini@zaclys.net>
2017-10-02 13:12:08 +0200

    Clean some unuseful array

--- arl/src/main/kotlin/Main.kt
+++ arl/src/main/kotlin/Main.kt
@@ -1,21 +1,23 @@
-import machine.learning.ARL;
+
+import machine.learning.ARL
 import java.io.File
+import java.util.*
 
 fun main(args : Array<String>) {
 
     // we open the .dat file located in data
     val myFile =  File("data/EURUSD.dat").inputStream()
-    var array2 : ArrayList<Double> = arrayListOf()
+    val array2 : ArrayList<Double> = arrayListOf()
 
     myFile.bufferedReader().useLines { lines -> lines.forEach {
         array2.add( it.split("/")[0].split(" ").last().toDouble())
     } }
 
-    array2.toDoubleArray().slice(0..2000)
-    val array = arrayListOf<Double>(1.99, 1.88, 1.77, 1.66, 1.55, 1.66, 1.77, 1.88, 1.77, 1.66, 1.55, 1.44, 1.33, 1.22, 1.11, 1.00)
+    val time = System.currentTimeMillis()
+    val arl = ARL(array2.toDoubleArray().slice(0..20000), 5)
 
-
-    var arl = ARL(array2.toDoubleArray().slice(0..10000), 4)
     arl.trainingLoop()
-    println(arl.toString())
+    println("${(System.currentTimeMillis() - time) / 1000} secondes" )
+
+//    println(arl.toString())
 }
