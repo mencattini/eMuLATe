4c1d5a236ed1f7ac44ab39c1bea4723731863832
Romain Mencattini <romain.mencattini@zaclys.net>
2017-10-04 11:28:10 +0200

    Add the reading from file in the test programme java

--- arl/src/main/java/Programme.java
+++ arl/src/main/java/Programme.java
@@ -1,19 +1,26 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
 import java.util.*;
 import machine.learning.ARL;
 
 public class Programme {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
 
         ArrayList array = new ArrayList<Double>();
+        Scanner lineReader = new Scanner(new File("data/EURUSD.dat"));
 
-        Random r = new Random();
-
-        for(int i=0; i < 15; i++){
-            array.add(r.nextDouble());
+        while(lineReader.hasNext()){
+            String line = lineReader.nextLine();
+            String[] tmp = line.split("/")[0].split(" ");
+            array.add(Double.parseDouble(tmp[tmp.length - 1]));
         }
-        ARL arl = new ARL(array, 4);
-        arl.trainingLoop(1);
+
+        ARL arl = new ARL(13);
+        arl.loop(array.subList(200000,260000), false, 1000);
+        arl.loop(array.subList(260000,270000), true, 1000);
 
         System.out.println(arl.toString());
     }
