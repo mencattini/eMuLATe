513480d3b75b71ca7dd1026f3511354351abdca7
Romain Mencattini <romain.mencattini@zaclys.net>
2017-10-10 13:12:20 +0200

    Add a loop to compute and plot multiple iteration

--- arl/src/main/kotlin/Main.kt
+++ arl/src/main/kotlin/Main.kt
@@ -2,23 +2,50 @@
 import machine.learning.ARL
 import java.io.File
 import java.util.*
+import koma.*
 
 fun main(args : Array<String>) {
 
     // we open the .dat file located in data
-    val myFile =  File("data/EURUSD.dat").inputStream()
-    val array2 : ArrayList<Double> = arrayListOf()
+    val myFile = File("data/EURUSD.dat").inputStream()
+    val array2: ArrayList<Double> = arrayListOf()
 
-    myFile.bufferedReader().useLines { lines -> lines.forEach {
-        array2.add( it.split("/")[0].split(" ").last().toDouble())
-    } }
+    myFile.bufferedReader().useLines { lines ->
+        lines.forEach {
+            array2.add(it.split("/")[0].split(" ").last().toDouble())
+        }
+    }
 
     val time = System.currentTimeMillis()
-    val arl = ARL(13)
+    val arl = ARL(10)
 
-    arl.loop(array2.toDoubleArray().slice(0..50000), false, 1000)
-    arl.loop(array2.toDoubleArray().slice(50000..60000), true, 1000)
-    println("time = ${(System.currentTimeMillis() - time) / 1000 }")
+    var i = 0
+    var p_t = arrayOf(0.0)
+    val step = 2000
+    val n = 100001
 
-    println(arl.toString())
+    while(i < n) {
+        println("$i")
+        arl.loop(array2.toDoubleArray().slice(i..i+step), 100)
+        p_t = arl.loop(array2.toDoubleArray().slice(i+step..i+step+500), 100, p_t)
+        arl.reset()
+        i += step
+
+    }
+
+    figure(1)
+    plot(p_t.toDoubleArray())
+    xlabel("ticks")
+    ylabel("p_t")
+    title("Run")
+
+    figure(2)
+    plot(array2.slice(0..n).toDoubleArray())
+    xlabel("ticks")
+    ylabel("value")
+    title("EURUSD")
+
+
+    println("time = ${(System.currentTimeMillis() - time) / 1000}")
+
 }
