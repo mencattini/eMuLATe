c5f029d72573ba22cbe288470294e586ecffbc62
Romain Mencattini <romain.mencattini@zaclys.net>
2017-10-12 10:09:11 +0200

    New class. Keep the position price

---
+++ arl/src/main/kotlin/machine/learning/PositionPrice.kt
@@ -0,0 +1,10 @@
+package machine.learning
+
+/**
+ * @author Romain Mencattini
+ *
+ * @param lastPositionPrice the last price on which we take position
+ * @param currentPrice the current price
+ */
+
+internal class PositionPrice(var lastPositionPrice: Double, var currentPrice: Double) {}
