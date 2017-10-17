35f5fe4e2835c686cc8a24f262f391a668d1677e
Romain Mencattini <romain.mencattini@zaclys.net>
2017-10-17 15:12:39 +0200

    Improve the stop trailing loss

--- arl/src/main/kotlin/machine/learning/ComputationalFunction.kt
+++ arl/src/main/kotlin/machine/learning/ComputationalFunction.kt
@@ -16,14 +16,14 @@ package machine.learning
  * @return a pair of signum and value
  */
 internal fun computeFt(givenT: Int, weight: Weights, oldFt: Double, sizeWindow: Int,
-                       returns: DoubleArray, parameters: Parameters, positionPrice: PositionPrice): Double {
+                       returns: DoubleArray, parameters: Parameters, positionProfit: PositionProfit): Double {
 
     // this part doesn't depends on index
     var sum = weight.wMplusOne() * oldFt + weight.vThreshold()
 
 
     // we get the useful weights and returns
-    // we add some 0 to avoid the out of bound array execption.
+    // we add some 0 to avoid the out of bound array exception.
     val usefulWeights: DoubleArray = weight.coefficients.sliceArray(0..givenT)
     val usefulReturns: DoubleArray = returns.plus(DoubleArray(sizeWindow))
             .sliceArray(maxOf(0,givenT - sizeWindow + 3)..givenT)
@@ -34,37 +34,23 @@ internal fun computeFt(givenT: Int, weight: Weights, oldFt: Double, sizeWindow: 
         sum += first * second
     }
 
-
-    val neutral = Math.signum(0.0)
-    val res :Double
-
+    var res = Math.signum(0.0)
     // we check the threshold
     // if it's greater than the threshold, we keep the result
     if (Math.abs(sum) < parameters.y) {
-        return neutral
+        return Math.signum(0.0)
     } else {
         res = Math.signum(sum)
     }
 
-    // if the ft doesn't change and it's not a 0.0, we need to check the loss
-    if (oldFt == res) {
-        // the difference between the currentPrice and the lastPositionPrice.
-        // if the different is negative, it means the trend goes down, if it's positive, the trend goes up
-        val diff = positionPrice.currentPrice - positionPrice.lastPositionPrice
-        // if it goes up, the good answer is +1, so positive times positive, => positive
-        // if it goes down, the good answer is -1, so negative, times negative => positive
-        // it means, if diff times position, i.e. {-1,+1} is negative, we do a bad choice and we need to controlate
-        // our loss.
-        if (diff * res < parameters.x * -0.001) {
-            // we change the signal, to get the opposite
-            return neutral
+    // stop trailing loss
+    if (Math.signum(res) == Math.signum(oldFt) && res != Math.signum(0.0)) {
+        // we need to check the loss if we keep the same position
+        if (positionProfit.lastPositionProfit - positionProfit.currentProfit < parameters.x * 0.001){
+            res = Math.signum(0.0)
         }
-    } else if (oldFt != res){
-        // if oldFt and res are different, and not neutral, it means we update the last position price
-        positionPrice.lastPositionPrice = positionPrice.currentPrice
-        return res
+    } else if (Math.signum(res) != Math.signum(oldFt)) {
+        positionProfit.lastPositionProfit = positionProfit.currentProfit
     }
-
-    // then we return the res
     return res
 }
