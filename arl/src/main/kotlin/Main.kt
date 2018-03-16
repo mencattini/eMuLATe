35c1af797b099a65428a0d288763b4dc85893513
Romain Mencattini <romain.mencattini@zaclys.net>
2018-03-16 15:00:52 +0100

    add some try/catch to avoid loop failure

--- arl/src/main/kotlin/Main.kt
+++ arl/src/main/kotlin/Main.kt
@@ -5,16 +5,8 @@ import kotlin.collections.ArrayList
 
 fun main(args : Array<String>) {
 
-//    // we open the .dat file located in data
-//    val myFile = File("data/EURUSD.dat").inputStream()
-//    val array2: ArrayList<Double> = arrayListOf()
-//
-//    myFile.bufferedReader().useLines { lines ->
-//        lines.forEach {
-//            array2.add(it.split("/")[1].split(" ")[0].toDouble())
-//        }
-//    }
-    val myFile = File("data/EURUSD_2000_2001_1m_bar.csv").inputStream()
+    // val myFile = File("data/EURUSD_2000_2001_1m_bar.csv").inputStream()
+    val myFile = File("data/2006-2010/gbpusd_2006-2010.csv").inputStream()
     val array2 : ArrayList<Double> = arrayListOf()
     myFile.bufferedReader().useLines {
         lines -> lines.forEach {
@@ -29,17 +21,26 @@ fun main(args : Array<String>) {
     var p_t = arrayOf(1.0)
     val step = 2000
     val stepLearn = 2500
-    val n = 50000
+    //val n = 50000
+    val n = 1662000
     val updateThreshold = 200
 
+    arl.initLogging()
     while(i < n) {
         println("$i")
-        arl.train(array2.toDoubleArray().slice(i..i+step), updateThreshold, p_t)
-        p_t = arl.test(array2.toDoubleArray().slice(i+step..i+stepLearn), p_t)
-        arl.reset()
+        try {
+            arl.train(array2.toDoubleArray().slice(i..i+step), updateThreshold, p_t)
+            p_t = arl.test(array2.toDoubleArray().slice(i+step..i+stepLearn), p_t)
+            arl.reset()
+            if (i % 50000 == 0) {
+                arl.saveInFile()
+            }
+        } catch (_ : ArrayIndexOutOfBoundsException){
+            arl.saveInFile()
+            break
+        }
         i += stepLearn - step
     }
-    arl.saveInFile()
 
     println("time = ${(System.currentTimeMillis() - time) / 1000}")
 
